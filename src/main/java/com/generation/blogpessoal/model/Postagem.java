8ea293597d1e707098541fc0d37fad6a5360ed67
Augusto Cesar <acsilvaluis@outlook.com>
2022-05-27 11:42:10 -0300

    Inicio Blog Pessoal

---
+++ src/main/java/com/generation/blogpessoal/model/Postagem.java
@@ -0,0 +1,78 @@
+package com.generation.blogpessoal.model;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import org.hibernate.annotations.UpdateTimestamp;
+
+
+@Entity
+@Table( name = "tb_postagens")
+public class Postagem
+{
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotBlank(message = "O atributo Título é Obrigatório e não pode utilizar espaços em branco!")
+	@Size(min = 5, max = 100, message = "O atributo Título deve conter no mínimo 05 e no máximo 100 caractéres")
+	private String titulo;
+	
+	@NotNull(message = "O atributo Texto é Obrigatório!")
+	@Size(min = 5, max = 100, message = "O atributo Texto deve conter no mínimo 10 e no máximo 1000 caractéres")
+	private String texto;
+	
+	@UpdateTimestamp
+	private LocalDateTime data;
+
+	public Long getId()
+	{
+		return id;
+	}
+
+	public void setId(Long id)
+	{
+		this.id = id;
+	}
+
+	public String getTitulo()
+	{
+		return titulo;
+	}
+
+	public void setTitulo(String titulo)
+	{
+		this.titulo = titulo;
+	}
+
+	public String getTexto()
+	{
+		return texto;
+	}
+
+	public void setTexto(String texto)
+	{
+		this.texto = texto;
+	}
+
+	public LocalDateTime getData()
+	{
+		return data;
+	}
+
+	public void setData(LocalDateTime data)
+	{
+		this.data = data;
+	}
+	
+	
+}
