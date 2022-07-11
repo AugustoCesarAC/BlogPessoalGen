847eb324a9b827cc556b228150e4bfbef6a44f11
Augusto Cesar <acsilvaluis@outlook.com>
2022-07-11 10:03:02 -0300

    Alteração na security

--- src/main/java/com/generation/blogpessoal/security/BasicSecurityConfig.java
+++ src/main/java/com/generation/blogpessoal/security/BasicSecurityConfig.java
@@ -161,6 +161,7 @@ public class BasicSecurityConfig extends WebSecurityConfigurerAdapter
 		 */
 
 		http.authorizeRequests()
+			.antMatchers("/**").permitAll()
 			.antMatchers("/usuarios/logar").permitAll()
 			.antMatchers("/usuarios/cadastrar").permitAll()
 			.antMatchers(HttpMethod.OPTIONS).permitAll()
